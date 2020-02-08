c7db710deccf5a657a12cb0c5d56c85527bb4144
paytonhaubrich <paytonhaubrich@10.0.0.21>
2020-02-08 10:28:14 -0500

    Ready for Review!

---
+++ Haubrich Homework 1/src/Homework1.java
@@ -0,0 +1,59 @@
+//Project:  Program 1
+//Author:   Payton Haubrich
+//Date:		Febuary 6, 2020
+//Version	1.0
+//Git User: phaubrich
+
+import java.util.Scanner;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Start Scanner		
+		Scanner keyboard= new Scanner (System.in); 
+		
+		// My Variables 
+		int weight; 
+		double medince;
+		short days;
+		
+		// Variable Numbers
+		weight= 150;
+		medince=.150;
+		days=24;
+		
+			
+		//Print And Println and output results
+		System.out.println("I am " + weight + " pounds.");
+		System.out.println("I am on " + medince + " Tylenon");
+		System.out.print("I have been taking it for " + days + " hours.");
+		
+		keyboard.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+				
+
+	}
+
+}
+
+
+
+
+
